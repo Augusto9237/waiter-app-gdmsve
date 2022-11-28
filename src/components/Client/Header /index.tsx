4eefaefb8e2cc61d3ddcaa6fd2d2d05b5671cfd7
Augusto Sousa <79643279+Augusto9237@users.noreply.github.com>
2022-11-28 02:50:11 +0000

    header cliente ok

--- src/components/Client/Header /index.tsx
+++ src/components/Client/Header /index.tsx
@@ -1,16 +1,28 @@
 import logo from "../../../assets/images/logo.svg";
 import { Container, Content } from "./styles";
 
-export function HeaderClient() {
+interface HeaderProps {
+  selectedTable: string;
+  onCancelOrder: () => void;
+}
+
+export function HeaderClient({ selectedTable, onCancelOrder }: HeaderProps) {
   return (
     <Container>
       <Content>
         <div className="page-details">
-          <span>Bem-vindo(a) ao</span>
-          <h1>REST</h1>
+          {!selectedTable && (
+            <>
+              <span>Bem-vindo(a) ao</span>
+              <h1>REST</h1>
+            </>
+          )}
+          {selectedTable && <h1>Pedido</h1>}
         </div>
-
-        <img src={logo} alt="waiterapp" />
+        {!selectedTable && <img src={logo} alt="waiterapp" />}
+        {selectedTable && (
+          <button onClick={onCancelOrder}>Cancelar Pedido</button>
+        )}
       </Content>
     </Container>
   );
